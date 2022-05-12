e5ca0323e1593f0fb5bb619c23d4b3c6a742c1ac
kalen510 <kalen.wiley@gmail.com>
2022-05-12 01:49:37 -0400

    scores js pseudocode

---
+++ assets/js/scores.js
@@ -0,0 +1,18 @@
+// define consts for existing elements
+
+
+// get stored scores from localstorage
+    // parse values back into objects
+
+// loop through scores to create elements
+    // create element
+    // assign values
+    // append to page
+
+
+// home button event listener
+
+
+// clear scores event listener
+    // clear localStorage
+    // reload page to reflect changes
