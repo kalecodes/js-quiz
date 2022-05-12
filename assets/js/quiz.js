1aedc02981155df1eaeb799d7bd3edf60b766788
kalen510 <kalen.wiley@gmail.com>
2022-05-11 21:22:31 -0400

    empty questions array

---
+++ assets/js/quiz.js
@@ -0,0 +1,53 @@
+// questions array
+const questions = [
+    {
+        question: '',
+        choices: ['','','',''],
+        answer: ''
+    },
+    {
+        question: '',
+        choices: ['','','',''],
+        answer: ''
+    },
+    {
+        question: '',
+        choices: ['','','',''],
+        answer: ''
+    },
+    {
+        question: '',
+        choices: ['','','',''],
+        answer: ''
+    },
+    {
+        question: '',
+        choices: ['','','',''],
+        answer: ''
+    },
+    {
+        question: '',
+        choices: ['','','',''],
+        answer: ''
+    },
+    {
+        question: '',
+        choices: ['','','',''],
+        answer: ''
+    },
+    {
+        question: '',
+        choices: ['','','',''],
+        answer: ''
+    },
+    {
+        question: '',
+        choices: ['','','',''],
+        answer: ''
+    },
+    {
+        question: '',
+        choices: ['','','',''],
+        answer: ''
+    }
+]
